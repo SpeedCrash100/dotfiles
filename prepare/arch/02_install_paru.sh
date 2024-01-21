455794f8e0bcacd97f94ad2211eac9f73ddd8ac7
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-01-21 11:40:12 +0300

    refactor: noconfirm for paru

--- prepare/arch/02_install_paru.sh
+++ prepare/arch/02_install_paru.sh
@@ -4,7 +4,10 @@ WORKDIR=$( mktemp -d )
 echo "pacman -S --noconfirm rustup bat devtools"
 sudo pacman -S --noconfirm rustup bat devtools
 
-(cd "${WORKDIR}" && git clone "https://aur.archlinux.org/paru.git" . && makepkg -si )
+(   \
+    cd "${WORKDIR}" && \
+    git clone "https://aur.archlinux.org/paru.git" . && \
+    makepkg -si --noconfirm )
 
 
 echo "paru" > "/tmp/PM"
