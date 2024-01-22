524bb1eaeac8ead99c3e15c6d0df81b925370bf8
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-01-22 20:15:28 +0300

    feat: add example of package

---
+++ packages/__template__/arch/post_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+#!
+#! The post-install scripts executes AFTER installing system.txt packages
+#!
+
+echo "Post-install hook for ${PACKAGE_NAME}"
---
+++ packages/__template__/arch/pre_install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#!
+#! The pre-install scripts executes BEFORE installing system.txt packages
+#!
+
+echo "Pre-install hook for ${PACKAGE_NAME}"
