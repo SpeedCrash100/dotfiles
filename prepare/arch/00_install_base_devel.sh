66451b2004ba308a44b48e9e29b13db1763e203f
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-01-22 20:03:17 +0300

    fix(arch): sync repos before install packages

--- prepare/arch/00_install_base_devel.sh
+++ prepare/arch/00_install_base_devel.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "pacman -Sy --noconfirm"
+sudo pacman -Sy --noconfirm
 
 echo "pacman -S --noconfirm --needed base-devel curl which"
 sudo pacman -S --noconfirm --needed base-devel curl which
