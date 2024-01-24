a8ab069934dd5fa15c5f812ccba49f2f34c46b13
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-01-24 20:53:23 +0300

    fix: set shell via usermod

--- packages/zsh/common/post_install.sh
+++ packages/zsh/common/post_install.sh
@@ -3,10 +3,6 @@ cp "${BASE_DIR}/packages/${PACKAGE_NAME}/common/.zshrc" "$HOME/"
 
 echo "Setting shell to zsh..."
 ZSH_EXE="$(which zsh)"
-if [ $? -eq 0 ]; then
-    chsh -s "${ZSH_EXE}"
-else
-    echo "Failed to set default shell" 1>&2;
-fi
+sudo usermod --shell "${ZSH_EXE}" "${USER}"
 
 
