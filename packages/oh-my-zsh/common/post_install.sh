1f3e094dafd65f3a133c81c02dba7a1d7f8ca45d
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-01-25 20:53:19 +0300

    feat(oh-my-zsh): add hooks for plugin management and more

--- packages/oh-my-zsh/common/post_install.sh
+++ packages/oh-my-zsh/common/post_install.sh
@@ -2,3 +2,9 @@
 
 echo "$(cat ${BASE_DIR}/packages/${PACKAGE_NAME}/common/.zshrc_add)" >> "$HOME/.zshrc"
 
+# Hooks for running before source $ZSH/oh-my-zsh.sh (Before init of oh-my-zsh)
+# Use it to add plugins
+mkdir -p "$HOME/.oh-my-zsh/custom/hooks/pre-oh-my-zsh.d"
+# Hooks to run after oh-my-zsh initialized
+#
+mkdir -p "$HOME/.oh-my-zsh/custom/hooks/late-oh-my-zsh.d"
