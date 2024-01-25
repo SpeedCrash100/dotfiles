e077c892167e853840eae8ce926d6bf777be30b0
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-01-25 20:46:56 +0300

    fix(zsh): get user name reliably for change shell

--- packages/zsh/common/post_install.sh
+++ packages/zsh/common/post_install.sh
@@ -3,6 +3,6 @@ cp "${BASE_DIR}/packages/${PACKAGE_NAME}/common/.zshrc" "$HOME/"
 
 echo "Setting shell to zsh..."
 ZSH_EXE="$(which zsh)"
-sudo usermod --shell "${ZSH_EXE}" "${USER}"
+sudo usermod --shell "${ZSH_EXE}" "$(id -nu $UID)"
 
 
