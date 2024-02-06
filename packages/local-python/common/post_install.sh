01bfb9bbe9cf87e0046b11d64c82e556fac618aa
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-02-06 14:39:41 +0300

    feat: Added local-python
    Creates venv with system python

---
+++ packages/local-python/common/post_install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo 'prepend_path "$HOME/.local/opt/python/bin"' > ~/.local/etc/profile.d/local-python.sh
---
+++ packages/local-python/common/pre_install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.local/opt/python
+python -m venv ~/.local/opt/python
