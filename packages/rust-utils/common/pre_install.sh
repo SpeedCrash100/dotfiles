6363d6b588e1ef3e6871d5363ab00d7c928b81a4
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-03-09 15:54:33 +0300

    feat: Added Rust-utils

---
+++ packages/rust-utils/common/pre_install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cargo install gitui --locked
+cargo install bottom --locked
+cargo install lsd --locked
+cargo install dust --locked
+cargo install hyperfine --locked
+
+# TODO: Add alias for bat to cat
+cargo install bat --locked
