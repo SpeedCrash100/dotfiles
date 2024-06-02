afe1b9eb8131699b96c4b4dc0b8c881d322d074c
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-06-02 15:55:59 +0300

    fix(deepfilternet): missing build instruction

--- packages/deepfilternet/common/install.sh
+++ packages/deepfilternet/common/install.sh
@@ -8,6 +8,7 @@ WORK_DIR=$(mktemp -d)
 
 (   cd "${WORK_DIR}" \
     && git clone https://github.com/Rikorose/DeepFilterNet.git . \
+    && $CARGO build --release -p deep-filter-ladspa \
     && install -D "${WORK_DIR}/target/release/libdeep_filter_ladspa.so" \
         "${HOME}/.local/lib/ladspa/libdeep_filter_ladspa.so" 
 
