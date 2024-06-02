1b070882498091f040a1c38fd3177b9fd98ce4ef
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-06-02 16:14:05 +0300

    fix(zsh): Unattended install of oh-my-zsh

--- packages/zsh/common/install.sh
+++ packages/zsh/common/install.sh
@@ -2,5 +2,5 @@
 $PM zsh
 
 if [ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then 
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 fi
